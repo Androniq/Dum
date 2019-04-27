6ea786e7c88f1c4ccf2e3d80b20b2f49a1402257
Andrij_Ghorbunov <androniq@gmail.com>
2019-04-27 14:49:07 +0300

    Continue working on approvals page

--- src/routes/Approvals/Approvals.js
+++ src/routes/Approvals/Approvals.js
@@ -4,11 +4,50 @@ import s from './Approvals.css';
 import cx from 'classnames';
 import { Helmet } from 'react-helmet';
 import withEverything from '../../withEverything';
+import FormattedText from '../../components/FormattedText/FormattedText';
+import { DEFAULT_USERPIC } from '../../utility';
+import BlueButton from '../../components/BlueButton/BlueButton';
 
 class Approvals extends React.Component
 {
+    clickApprove(item)
+    {
+    }
+
+    clickDeny(item)
+    {
+    }
+
+    clickViewUser(item)
+    {
+    }
+
+    clickViewArticle(item)
+    {
+    }
+
+    clickBan(item)
+    {
+    }
+
     render()
     {
+        this.props.data.proposedArgs.forEach(proposedArg =>
+            {
+                var articleId = proposedArg.Article;
+                var article = this.props.data.articles.find(it => it.ID === articleId);
+                proposedArg.ArticleInst = article;
+                var voteId = proposedArg.Vote;
+                var vote = this.props.data.votes.find(it => it.ID === voteId);
+                proposedArg.VoteInst = vote;
+                proposedArg.voteDescription = vote.ShortDescriptionTemplate.replace('%A%', article.ShortA).replace('%B%', article.ShortB);
+                var priorityId = proposedArg.Priority;
+                var priority = this.props.data.priorities.find(it => it.ID == priorityId);
+                proposedArg.PriorityInst = priority;
+                var userId = proposedArg.Owner;
+                var user = this.props.data.users.find(it => it._id === userId);
+                proposedArg.User = user;
+            });
         return (
             <div className="container">
                 <Helmet>
@@ -17,7 +56,32 @@ class Approvals extends React.Component
                 <div className={s.itemList}>
                     {this.props.data.proposedArgs.map(item => (
                         <div key={item._id} className={s.itemContainer}>
-                            <span>{item.Content}</span>
+                            <div className={s.labelContainer}>
+                                <span className={cx(s.labelBase, s.labelTokenA)}>{item.ArticleInst.TokenA}</span>
+                                <span className={cx(s.labelBase, s.labelTokenB)}>{item.ArticleInst.TokenB}</span>
+                            </div>
+                            <div className={s.labelContainer}>
+                                <span className={cx(s.labelBase, s.labelVote)}>{item.voteDescription}</span>
+                                <span className={cx(s.labelBase, s.labelPriority)}>{item.PriorityInst.Title}</span>
+                            </div>
+                            <div className={s.contentContainer}>
+                                <FormattedText html={item.Content} />
+                            </div>
+                            <div className={s.panel}>
+                                <div className={s.avatar}>
+                                    <img src={item.User && item.User.photo || DEFAULT_USERPIC} className="userpic" />
+                                </div>
+                                <div className={s.username}>
+                                    <span>{item.User && item.User.displayName || "Анонім"}</span>
+                                </div>
+                                <BlueButton onClick={(()=>this.clickViewUser(item)).bind(this)}>Переглянути користувача</BlueButton>
+                                <BlueButton onClick={(()=>this.clickViewArticle(item)).bind(this)}>Переглянути статтю</BlueButton>
+                            </div>
+                            <div className={s.panel}>
+                                <BlueButton onClick={(()=>this.clickApprove(item)).bind(this)}>Схвалити</BlueButton>
+                                <BlueButton onClick={(()=>this.clickDeny(item)).bind(this)}>Відхилити</BlueButton>
+                                <BlueButton onClick={(()=>this.clickBan(item)).bind(this)}>БАН!</BlueButton>
+                            </div>
                         </div>
                     ))}
                 </div>
--- src/serverLogic/getApprovals.js
+++ src/serverLogic/getApprovals.js
@@ -18,6 +18,8 @@ import {
 	USER_LEVEL_ADMIN,
     USER_LEVEL_OWNER } from '../utility';
 
+const ObjectID = require('mongodb').ObjectID;
+
 export default async function getApprovals(user)
 {
     if (!checkPrivilege(user, USER_LEVEL_MODERATOR))
@@ -25,9 +27,40 @@ export default async function getApprovals(user)
         return { status: 403, message: "Need moderator rights to see approvals list" };
     }
     var proposedArgs = await mongoAsync.dbCollections.proposedArguments.find().toArray();
+    var articleIds = [];
+    var userIds = [];
+    var userIds2 = [];
+    proposedArgs.forEach(proposedArg =>
+        {
+            if (!articleIds.some(item => item === proposedArg.Article))
+            {
+                articleIds.push(proposedArg.Article);
+            }
+            if (!userIds.some(item => item === proposedArg.Owner))
+            {
+                userIds.push(proposedArg.Owner);
+                userIds2.push(new ObjectID(proposedArg.Owner));
+            }
+        });
+    var articles = await mongoAsync.dbCollections.articles.find({ ID: { $in: articleIds }}).toArray();
+    articles.forEach(article => {
+        article.Content = null;
+    });
+    var users = await mongoAsync.dbCollections.users.find({ _id: { $in: userIds2 }},
+        { projection: { "photo": 1, "displayName": 1 } }).toArray();
+    var votes = mongoAsync.preloads.votes;
+    var priorities = mongoAsync.preloads.priorities;
+    votes.forEach(vote =>
+    {
+        vote.ShortDescription = null;
+    });
     var data =
     {
-        proposedArgs
+        proposedArgs,
+        articles,
+        votes,
+        priorities,
+        users
     };
     return data;
 }
